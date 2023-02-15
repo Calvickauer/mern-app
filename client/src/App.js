e10a1cfa8feb59c4c5fb3af841aa5e8ebffc3ab8
calvickauer <calvickauer@gmail.com>
2023-02-15 10:32:28 -0800

    merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,58 @@
 import logo from './logo.svg';
+import {React, useState} from 'react';
 import './App.css';
 
 function App() {
   const {name, setName} = useState('');
   const {email, setEmail} = useState('');
   const {password, setPassword} = useState('');
-  
+
+  async function registerUser(e){
+    e.preventDefault();
+
+    const response = await fetch('http://localhost:2500/api/register', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    })
+    const data = await response.json();
+    console.log(data);
+  }
+
+
   return (
     <div>
       <h1>Register</h1>
-      <form>
+      <form onSubmit={registerUser()}>
+        <br />
         <input
           value={name}
           onChange={(e) => setName(e.target.value)}
           type="text"
           placeholder='Name' 
         />
+        <br />
         <input
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           type="text"
           placeholder='email' 
         />
+        <br />
         <input
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           type="text"
           placeholder='Password' 
         />
+        <br />
+        <input type='submit' value="Register" />
       </form>
     </div>
 );
